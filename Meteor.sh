c5643ef85e2efa781a3f40d7ec1ba5972c43592e
junwoojo <78505823+JUNWOOJOK@users.noreply.github.com>
2023-02-17 20:24:44 +0900

    Update Meteor.sh

--- Meteor.sh
+++ Meteor.sh
@@ -3,3 +3,6 @@ ruby /home/junwoojo/Meteor/meteor-master/data_preparation_tools/MeteorImportFast
 
 
 ruby /home/junwoojo/Meteor/meteor-master/data_preparation_tools/MeteorReferenceBuilder.rb -i gene_catalogue.fna -p /home/junwoojo/Project_Darkmatter/Reference/ -n gene_catalogue
+
+#runmeteor
+for i in $(ls|grep TCGA);do meteor.rb -w ../../workflow/Darkmatter.ini  -p /home/junwoojo/Project_Darkmatter/Darkmatter/ -o mapping -i ../sample/${i};done
