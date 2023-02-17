155c4e0c1ca835e0fc52e73f2b86770df4eb1676
junwoojo <78505823+JUNWOOJOK@users.noreply.github.com>
2023-02-17 20:16:12 +0900

    Create Meteor.sh

---
+++ Meteor.sh
@@ -0,0 +1,5 @@
+ruby /home/junwoojo/Meteor/meteor-master/data_preparation_tools/MeteorImportFastq.rb -i /home/junwoojo/Project_Darkmatter/Darkmatter/sample/ -p Darkmatter -t Illumina -m TCGA -d
+
+
+
+ruby /home/junwoojo/Meteor/meteor-master/data_preparation_tools/MeteorReferenceBuilder.rb -i gene_catalogue.fna -p /home/junwoojo/Project_Darkmatter/Reference/ -n gene_catalogue
